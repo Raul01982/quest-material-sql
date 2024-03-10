6b50c2b8089001f66c3a4d24a7ce371bf3c83577
Raul01982 <161337310+Raul01982@users.noreply.github.com>
2024-03-10 21:35:10 +0100

    Update am_db.sql

--- db.sql
+++ db.sql
@@ -9,3 +9,13 @@ CREATE TABLE `wild_db_quest`.`wizard`
     `is_muggle`   BOOLEEN      NOT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `wild_db_quest`.`school`
+(
+    `id`          INT          NOT NULL AUTO_INCREMENT,
+    `name`        VARCHAR(100) NOT NULL,
+    `capacity`    INT          NULL,
+    `country` VARCHAR(255)     NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
